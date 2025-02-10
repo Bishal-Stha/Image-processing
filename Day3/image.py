eaecb777261970f7096bfd2358786a7f09414d36
Bishal-Stha <programmerbishal@gmail.com>
2025-02-10 10:39:15 +0545

    Last day of Image Processing. Thank you Bibash dai for this amazing workshop.

--- Day3/image.py
+++ Day3/image.py
@@ -1,5 +1,12 @@
 import cv2
 
-image = cv2.imread("./../Day2/batman.png")
+image = cv2.imread("./images/ORL(new)/s1/1.pgm")
 image = cv2.resize(image,(500,500))
 
+image_blur = cv2.GaussianBlur(image,(25,25),0)
+
+cv2.imshow("Original",image)
+cv2.imshow("Output",image_blur)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
